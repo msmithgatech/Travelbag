1019d6fd4b464ad6df909aac1c4998301c262cd9
Mamie Smith <girlinmywhirl@gmail.com>
2018-06-04 19:20:44 -0400

    checking push

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var db = require("./models");
+var db = require('models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,9 +15,9 @@ app.use(express.static("public"));
 
 //  ROUTES
 
-require("./routes/html-routes.js")(app);
-require("./routes/traveler-routes.js")(app);
-require("./routes/checklist-routes.js")(app);
+require("/routes/html-routes.js")(app);
+require("/routes/traveler-routes.js")(app);
+require("/routes/checklist-routes.js")(app);
 
 
 // ===========================================================
